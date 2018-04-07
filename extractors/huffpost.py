3150ede36ebb320230908da633934929d058f1ba
coder_h <harsh.sinha@students.iiit.ac.in>
2018-04-07 23:57:46 +0530

    Adding images to DB

--- extractors/huffpost.py
+++ extractors/huffpost.py
@@ -1,6 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 from inserttodatabase import push_to_database
+import urllib.request
+import os
+from PIL import Image
+from time import time
+
 url = 'https://www.huffingtonpost.in/2018/04/06/wont-vote-for-him-again-residents-of-a-small-rajasthan-town-have-a-warning-for-modi-ahead-of-the-2019-polls_a_23404420/?utm_hp_ref=in-homepage'
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
@@ -8,6 +13,18 @@ headline = (soup.find('h1',class_='headline__title')).get_text().strip()
 subtitle = (soup.find('h2',class_='headline__subtitle')).get_text().strip()
 para = soup.find("div","post-contents").find_all("p")
 story=""
+
+
+image = (soup.find_all('img',class_='image__src'))
+for i in image:
+	cur=i['src']
+	fullfilename = os.path.join('../static/', str(time())+".jpg")
+	urllib.request.urlretrieve(cur,fullfilename)
+	img = Image.open(fullfilename)
+	width, height = img.size
+	img = img.resize((320,int((320*height)/width)), Image.ANTIALIAS)
+	img.save(fullfilename)
+
 for x in para:
 	if x.string is not None:
 		story=story+"<p>"+x.get_text().strip()+"</p>"
