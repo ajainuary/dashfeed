68c2b687afa2abacc319f7612cadb2999fa934fa
coder_h <harsh.sinha@students.iiit.ac.in>
2018-04-08 23:24:36 +0530

    Update DB

--- extractors/bbc.py
+++ extractors/bbc.py
@@ -3,40 +3,33 @@ from bs4 import BeautifulSoup
 from inserttodatabase import push_to_database
 import urllib.request
 import os
-from PIL import Image
 from time import time
 
-
-url = 'http://www.bbc.com/news/world-asia-india-43652304'
+url = 'http://www.bbc.com/news'
 response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-
-image = (soup.find('img',class_='js-image-replace'))['src']
-fullfilename = os.path.join('../static/', str(time())+".jpg")
-urllib.request.urlretrieve(image,fullfilename)
-img = Image.open(fullfilename)
-width, height = img.size
-img = img.resize((320,(320*height)/width), Image.ANTIALIAS)
-
-headline = (soup.find('h1',class_='story-body__h1')).get_text().strip()
-subtitle = (soup.find(class_='story-body__introduction')).get_text().strip()
-para = soup.find("div","story-body__inner").find_all("p")
-story=""
-
-
-save = ""
-image = (soup.find_all('img',class_='js-image-replace'))
-for i in image:
-	cur=i['src']
-	fullfilename = os.path.join('../static/', str(time())+".jpg")
-	urllib.request.urlretrieve(cur,fullfilename)
-	img = Image.open(fullfilename)
-	width, height = img.size
-	img = img.resize((320,int((320*height)/width)), Image.ANTIALIAS)
-	img.save(fullfilename)
-	save = save + str(fullfilename)
-	save = save + ","
-for x in para:
-	if x.string is not None:
-		story=story+"<p>"+x.get_text().strip()+"</p>"
-push_to_database(headline,subtitle,story,1,url,save)
+re = BeautifulSoup(response.content, 'html.parser')
+links = re.find_all('a',class_='gs-c-promo-heading nw-o-link-split__anchor gs-o-faux-block-link__overlay-link gel-pica-bold')
+for i in links:
+	try:
+		url = 'http://www.bbc.com'+i['href']
+		newresponse = requests.get(url)
+		soup = BeautifulSoup(newresponse.content,'html.parser')
+		headline = (soup.find('h1',class_='story-body__h1')).get_text().strip()
+		subtitle = (soup.find(class_='story-body__introduction')).get_text().strip()
+		para = soup.find("div","story-body__inner").find_all("p")
+		story=""
+		save = ""
+		image = (soup.find_all('img',class_='js-image-replace'))
+		for i in image:
+			cur=i['src']
+			curtime = str(time())
+			fullfilename = os.path.join('../site/static', curtime+".jpg")
+			urllib.request.urlretrieve(cur,fullfilename)
+			save = save + str(curtime+".jpg")
+			save = save + ","
+		for x in para:
+			if x.string is not None:
+				story=story+"<p>"+x.get_text().strip()+"</p>"
+		push_to_database(headline,subtitle,story,1,url,save)
+	except:
+		pass
