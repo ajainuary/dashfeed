9c0f4a91475bf257a9b22fc6134388b5d1a744a5
Anurag Jain <ajainuary@gmail.com>
2018-04-08 18:53:02 +0530

    Flask Application started

--- site/index.py
+++ site/index.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask import render_template
+import sqlite3
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-	return "Hello, World!"
+@app.route('/article/<int:id>')
+def article(id):
+	info = sqlite3.connect('../news.db')
+	cursor = info.cursor()
+	cursor.execute("SELECT * FROM NEWS WHERE id = %d" % id)
+	cont = cursor.fetchone()
+	return render_template('article.html', title=cont[1], content=cont[3])
