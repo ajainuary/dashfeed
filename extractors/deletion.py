fae6761627aa1bfa23aa38f1d87380fd81f480bf
coder_h <harsh.sinha@students.iiit.ac.in>
2018-04-11 12:33:03 +0530

    Database done

--- extractors/deletion.py
+++ extractors/deletion.py
@@ -2,7 +2,7 @@ import sqlite3
 connection = sqlite3.connect('../news.db')
 cursor=connection.cursor() 
 try:
-	cursor.execute("delete from NEWS where id>=31 and id<=48")
+	cursor.execute("delete from NEWS where id>=139 and id<=153")
 	connection.commit()
 finally:
 	connection.close()
--- extractors/ecotime.py
+++ extractors/ecotime.py
@@ -4,11 +4,12 @@ from inserttodatabase import push_to_database
 import urllib.request
 import os
 from time import time
+import re
 
 url = 'https://economictimes.indiatimes.com/'
 response = requests.get(url)
-re = BeautifulSoup(response.content, 'html.parser')
-aux = re.find('div',class_='tabsContent')
+ret = BeautifulSoup(response.content, 'html.parser')
+aux = ret.find('div',class_='tabsContent')
 links = aux.find_all('li')
 for i in links:
 	try:
@@ -23,6 +24,13 @@ for i in links:
 		story = ""
 		story = soup.find("div",class_="Normal").get_text().strip()
 		image = soup.find_all('figure')
+		sub = soup.find_all('div',class_='flt navElement relNav')
+		for i in sub:
+			try:
+				subtitle = i.find('a',class_='current').get_text().strip()
+			except:
+				pass
+		subtitle = subtitle.lower()
 		for i in image:
 			temp = i.find('img')
 			cur = temp['src']
@@ -34,6 +42,12 @@ for i in links:
 			save = save + ","
 		if save == "" or story == "":
 			raise Exception('No image')
-		push_to_database(headline,subtitle,para,1,url,'business',save)
+		if subtitle == 'tech' or subtitle == 'ites' or subtitle == 'hardware' or subtitle == 'software' or subtitle == 'internet' or subtitle == 'tech and gadgets':
+			subtitle = 'technology'
+		if subtitle == 'industry' or subtitle == 'markets' or subtitle == 'jobs' or subtitle == 'stocks' or subtitle == 'startups' or subtitle == 'energy' or subtitle == 'economy' or subtitle == 'company':
+			subtitle = 'business'
+		if subtitle == 'news' or subtitle == 'international':
+			subtitle = 'world'
+		push_to_database(headline,subtitle,para,1,url,subtitle,save)
 	except:
 		pass
