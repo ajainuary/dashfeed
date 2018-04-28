2c966fce985775981d4fb452e2fd064f91359209
Harsh <harsh.26020@gmail.com>
2018-04-28 23:55:36 +0530

    Added function for registration

--- login.py
+++ login.py
@@ -3,8 +3,8 @@ import sqlite3
 def login(email,password):
 	conn = sqlite3.connect('user.db')
 	cursor = conn.cursor()
-	sql = """select from user where email like "%{p}%"; """
-	sql = sql.format(p=category)
+	sql = """select from user where email="{p}" and password="{q}" ; """
+	sql = sql.format(p=email,q=)
 	ro = cursor.execute(sql)
 	customers = ro.fetchall()
 	return customers
