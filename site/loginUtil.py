80c6b6357221c3833ceccbfa0eb32c3381b8375d
Harsh <harsh.26020@gmail.com>
2018-04-29 18:49:56 +0530

    Session created

--- site/loginUtil.py
+++ site/loginUtil.py
@@ -1,7 +1,7 @@
 import sqlite3
 def validate(username, password):
     con = sqlite3.connect('../news.db')
-    completion = False
+    completion = None
     with con:
         cur = con.cursor()
         cur.execute("SELECT * FROM user")
@@ -12,7 +12,8 @@ def validate(username, password):
              dbPass = row[2]
              if dbUser==username:
                  completion=check_password(dbPass, password)
+                 if completion:
+                 	return row[0]
     return completion
 def check_password(dbPass,password):
-    print(dbPass, password)
     return dbPass == password
